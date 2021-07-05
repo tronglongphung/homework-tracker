fd7482cc7a1ca0c80b1c5d3744055de9d9d7c7b7
tronglongphung <tronglongphung@gmail.com>
2021-07-05 19:59:35 +0930

    fixing routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
-router.get("/workouts", () => {});
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,10 +13,11 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
