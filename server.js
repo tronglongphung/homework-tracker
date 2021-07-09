27ed8c4e2809fa886461ef667325fa7c7c58ee22
tronglongphung <tronglongphung@gmail.com>
2021-07-10 04:06:30 +0930

    minor changes

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
